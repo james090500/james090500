8afdc733c458ec67ebedc911eb5aa0d361f9bd92
James Harrison <james@fasttortoise.co.uk>
2022-07-26 15:31:03 +0100

    👍 More work with URLs

--- page/vite.config.js
+++ page/vite.config.js
@@ -1,11 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
+import process  from 'node:process'
 
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        createHtmlPlugin({ inject: { data: { BASE_URL: process.env.CF_PAGES_URL ?? "http://localhost:5173" }}})
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
